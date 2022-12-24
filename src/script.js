9d1ad7ba8bb5c755451ca96c1e9eeb31b7288db2
callme-devil <pransermi@gmail.com>
2022-12-24 18:21:18 +0330

    feat: Added textures

--- src/script.js
+++ src/script.js
@@ -2,7 +2,6 @@ import './style.css'
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import GUI from 'lil-gui'
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
 
 /**
  * Base
@@ -16,95 +15,121 @@ const canvas = document.querySelector('canvas.webgl')
 // Scene
 const scene = new THREE.Scene()
 
-
+/**
+ * Textures
+ */
+const textureLoader = new THREE.TextureLoader()
 
 /**
- * GLTF Loader
+ * House
  */
-
-const gltfLoader = new GLTFLoader()
-
-gltfLoader.load(
-    'models/Ghost/untitled.glb',
-    (gltf)=>{
-        // while(gltf.scene.children.length){
-
-        //     scene.add(gltf.scene.children[0])
-            
-        // }
-        scene.add(gltf.scene)
-
-        // gltf.scene.scale.set(0.04, 0.02 ,0.02)
-        // gltf.scene.position.z = -12
-
-        // scene.add(gltf.scene)
-
-        // gltf.scene.scale.set(2,2 , 2)
-        gltf.scene.rotation.y =100 
-
-        gltf.scene.scale.set(0.2,0.2,0.2)
-        gltf.scene.rotation.x = 1.3
-        gltf.scene.rotation.y = - 0.01
-
-      
-
-
-    }
-)
-
-gltfLoader.load(
-    'models/Graves/grave.glb',
-    (gltf2)=>{
-        scene.add(gltf2.scene)
-
-        gltf2.scene.scale.set(0.04 , 0.03 , 0.03)
-        gltf2.scene.position.x = 1
-    }
-)
-
-gltfLoader.load(
-    'models/Graves/grave2.glb',
-    (gltf3)=>{
-        scene.add(gltf3.scene)
-
-        gltf3.scene.scale.set(0.04 , 0.03 , 0.03)
-        gltf3.scene.position.x = 1
-    }
-)
-
-
+const house = new THREE.Group()
+scene.add(house)
 
 /**
- * Floor
+ * Walls
  */
-const floor = new THREE.Mesh(
-    new THREE.PlaneGeometry(10, 10),
+
+const walls = new THREE.Mesh(
+    new THREE.BoxGeometry(4 , 2.5 , 4),
     new THREE.MeshStandardMaterial({
-        color: '#444444',
-        metalness: 0,
-        roughness: 0.5
+        color: "#ac8e82"
     })
 )
-floor.receiveShadow = true
+walls.position.y = 2.5 / 2
+house.add(walls)
+
+/**
+ * Roof
+ */
+
+const roof = new THREE.Mesh(
+    new THREE.ConeGeometry(3.5 , 1 , 4),
+    new THREE.MeshStandardMaterial({
+        color: '#b35f45'
+    })
+)
+roof.position.y = 2.5 + 0.5
+roof.rotation.y = Math.PI * 0.25
+
+house.add(roof)
+
+/**
+ * Door
+ */
+const door = new THREE.Mesh(
+    new THREE.PlaneGeometry(2 , 2),
+    new THREE.MeshStandardMaterial({
+        color : "#b35f45"
+    })
+)
+door.position.z = 2 + 0.01
+door.position.y = 1
+
+house.add(door)
+
+/**
+ * Bushes
+ */
+const bushGeometry = new THREE.SphereGeometry(1 , 16 , 16)
+const bushMaterial = new THREE.MeshStandardMaterial({
+    color: '#89c854'
+})
+
+const bush1 = new THREE.Mesh(bushGeometry , bushMaterial)
+bush1.scale.set(0.4 , 0.4 , 0.4)
+bush1.position.set(1.2 , 0.3 , 2.35)
+
+const bush2 = new THREE.Mesh(bushGeometry , bushMaterial)
+bush2.scale.set(0.25 , 0.25 , 0.25)
+bush2.position.set(1.7 , 0.2 , 2.25)
+
+const bush3 = new THREE.Mesh(bushGeometry , bushMaterial)
+bush3.scale.set(0.4 , 0.4 , 0.4)
+bush3.position.set(-1.2 , 0.3 , 2.35)
+
+const bush4 = new THREE.Mesh(bushGeometry , bushMaterial)
+bush4.scale.set(0.25 , 0.25 , 0.25)
+bush4.position.set(-1.7 , 0.2 , 2.25)
+
+house.add(bush1 , bush2 , bush3 , bush4)
+
+/**
+ * Graves
+ */
+const graves = new THREE.Group()
+scene.add(graves)
+
+const graveGeometry = new THREE.BoxGeometry(0.6 , 0.8 , 0.2)
+const graveMaterial = new THREE.MeshStandardMaterial({
+    color: "#b2b6b1"
+})
+
+// Floor
+const floor = new THREE.Mesh(
+    new THREE.PlaneGeometry(20, 20),
+    new THREE.MeshStandardMaterial({ color: '#a9c388' })
+)
 floor.rotation.x = - Math.PI * 0.5
+floor.position.y = 0
 scene.add(floor)
 
 /**
  * Lights
  */
-const ambientLight = new THREE.AmbientLight(0xffffff, 5.8)
+// Ambient light
+const ambientLight = new THREE.AmbientLight('#ffffff', 0.5)
+gui.add(ambientLight, 'intensity').min(0).max(1).step(0.001)
 scene.add(ambientLight)
 
-const directionalLight = new THREE.DirectionalLight(0xffffff, 0.6)
-directionalLight.castShadow = true
-directionalLight.shadow.mapSize.set(1024, 1024)
-directionalLight.shadow.camera.far = 15
-directionalLight.shadow.camera.left = - 7
-directionalLight.shadow.camera.top = 7
-directionalLight.shadow.camera.right = 7
-directionalLight.shadow.camera.bottom = - 7
-directionalLight.position.set(5, 5, 5)
-scene.add(directionalLight)
+// Directional light
+const moonLight = new THREE.DirectionalLight('#ffffff', 0.5)
+moonLight.position.set(4, 5, - 2)
+gui.add(moonLight, 'intensity').min(0).max(1).step(0.001)
+gui.add(moonLight.position, 'x').min(- 5).max(5).step(0.001)
+gui.add(moonLight.position, 'y').min(- 5).max(5).step(0.001)
+gui.add(moonLight.position, 'z').min(- 5).max(5).step(0.001)
+scene.add(moonLight)
 
 /**
  * Sizes
@@ -134,12 +159,13 @@ window.addEventListener('resize', () =>
  */
 // Base camera
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
-camera.position.set(2, 2, 2)
+camera.position.x = 4
+camera.position.y = 2
+camera.position.z = 5
 scene.add(camera)
 
 // Controls
 const controls = new OrbitControls(camera, canvas)
-controls.target.set(0, 0.75, 0)
 controls.enableDamping = true
 
 /**
@@ -148,8 +174,6 @@ controls.enableDamping = true
 const renderer = new THREE.WebGLRenderer({
     canvas: canvas
 })
-renderer.shadowMap.enabled = true
-renderer.shadowMap.type = THREE.PCFSoftShadowMap
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 
@@ -157,13 +181,10 @@ renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
  * Animate
  */
 const clock = new THREE.Clock()
-let previousTime = 0
 
 const tick = () =>
 {
     const elapsedTime = clock.getElapsedTime()
-    const deltaTime = elapsedTime - previousTime
-    previousTime = elapsedTime
 
     // Update controls
     controls.update()
