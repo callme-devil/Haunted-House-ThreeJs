9518d20f6f0851e15632db342112d22de3226f25
callme-devil <pransermi@gmail.com>
2022-12-24 18:38:24 +0330

    feat: Added graves

--- src/script.js
+++ src/script.js
@@ -101,9 +101,26 @@ const graves = new THREE.Group()
 scene.add(graves)
 
 const graveGeometry = new THREE.BoxGeometry(0.6 , 0.8 , 0.2)
+const grave2Geometry = new THREE.BoxGeometry(0.6 , 0.8 , 0.2)
 const graveMaterial = new THREE.MeshStandardMaterial({
     color: "#b2b6b1"
 })
+
+for(let i = 0; i < 65; i++){
+    const angle = Math.random() * Math.PI * 7
+    const radius = 4 + Math.random() * 5.6
+
+    const x = Math.sin(angle) * radius
+    const z = Math.cos(angle) * radius + 0.02
+
+    const grave = new THREE.Mesh(graveGeometry , graveMaterial)
+
+    grave.position.set(x , 0.4 , z)
+    grave.rotation.y = (Math.random() - 0.5) * 0.4
+
+    graves.add(grave)
+}
+
 
 // Floor
 const floor = new THREE.Mesh(
